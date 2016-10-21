743628721d81687693ed9bdbeb972eb28fd01bf1
Matt Amos <zerebubuth@gmail.com>
2016-10-21 16:00:38 +0100

    Changing how multiple valued config options are passed.

--- attributes/default.rb
+++ attributes/default.rb
@@ -9,7 +9,7 @@ default[:xonacatl][:listen] = ':80'
 default[:xonacatl][:patterns] = {
   '/{layers}/{z:[0-9]+}/{x:[0-9]+}/{y:[0-9]+}.{fmt}' => 'http://upstream.tile.server/{layers}/{z:[0-9]+}/{x:[0-9]+}/{y:[0-9]+}.{fmt}'
 }
-default[:xonacatl][:headers] = []
+default[:xonacatl][:headers] = {}
 
 default[:xonacatl][:runit][:timeout] = 60
 default[:xonacatl][:user][:create_group] = true
