345b370f0d2264ff501b3842c5218026a9de04d3
Matt Amos <zerebubuth@gmail.com>
2016-10-24 13:13:40 +0100

    Add support for configuring headers which aren't to be forwarded.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'matt.amos@mapzen.com'
 license          'MIT'
 description      'Installs/Configures xonacatl'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.4'
+version          '0.1.5'
 
 recipe 'xonacatl', 'Installs Xonacatl'
 
